8325911c217735688f414cb5db1c296edaa4277f
jbenua <harumm.scarumm@gmail.com>
2016-02-02 17:33:09 +0300

    old files restructured

--- interface.py
+++ interface.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from StartScreen import StartScreen
+# from StartScreen import StartScreen
 from modules.User import User
 import sys
 from PyQt4.QtGui import *
@@ -10,18 +10,16 @@ ICON = "img/emotiv_icon.png"
 
 
 if __name__ == "__main__":
-    root = Tk()
-    icon = PhotoImage(file=ICON)
-    root.tk.call('wm', 'iconphoto', root._w, icon)
-    root.title("Biomeh")
+    # root = Tk()
+    # icon = PhotoImage(file=ICON)
+    # root.tk.call('wm', 'iconphoto', root._w, icon)
+    # root.title("Biomeh")
     user = User()
-    ss = StartScreen(root, user)
-    root.mainloop()
+    # ss = StartScreen(root, user)
+    # root.mainloop()
 
     # pyqt
     app = QApplication(sys.argv)
     main_window = MainWindow(user)
 
     sys.exit(app.exec_())
-    # app.exec_()
-    print("EXITING")
